9415303692b2ae9f075088e549496c5c253e26f7
Matthew Day <daystar.1998@yahoo.com>
2025-06-18 12:42:42 -0500

    Automatically open devtools when running with the SDK version of NWJS

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 // main.js
 //=============================================================================
 
+if(process.versions["nw-flavor"] === "sdk") {
+    require('nw.gui').Window.get().showDevTools();
+}
+
 PluginManager.setup($plugins);
 
 window.onload = function() {
